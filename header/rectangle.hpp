40b0f9940fc62e4d12a2c6bf582d8599db97b4f8
JakinChan200 <JakinChan200@gmail.com>
2020-11-11 21:22:21 -0800

    Finished the rectangle.hpp file and imported googletest

--- header/rectangle.hpp
+++ header/rectangle.hpp
@@ -6,13 +6,27 @@ class Rectangle {
         int width;
         int height;
     public:
-        Rectangle();
-        Rectangle(int w,int h);
+        Rectangle() {
+	    set_width(0);
+	    set_height(0);
+	}
+        Rectangle(int w,int h) {
+	    set_width(w);
+	    set_height(h);
+	}
 
-        void set_width(int w);
-        void set_height(int h);
-        int area();
-        int perimeter();
+        void set_width(int w) {
+	    this->width = w;
+	}
+        void set_height(int h) {
+	    this->height = h;
+	}
+        int area() {
+	    return width * height;
+	}
+        int perimeter() {
+	    return 2*(width + height);
+	}
 };
 
 #endif // RECTANGLE_HPP
